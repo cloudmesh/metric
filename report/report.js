62f35f308077f50f08a72a573b39381080e5071a
Tim Whitson <whitstd@gmail.com>
2017-03-07 19:15:23 -0500

    add chart pagination

--- report/report.js
+++ report/report.js
@@ -1,12 +1,22 @@
 $(function () {
-    $('#containerDistType').highcharts({
+    var series = [{
+        name: 'Publication Type Percentage',
+        data: [
+            {name:'Journal Paper', y:6321},
+            {name:'None', y:3266},
+            {name:'Conference Paper', y:492},
+            {name:'Book Chapter', y:52},
+            {name:'Book', y:6}
+        ]
+    }];
+      
+    opts = {
         chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
+            type: 'pie',
+            animation: false
         },
         title: {
-            text: 'XD Related Publications By Publication Type'
+            text: 'XD-Related Publications By Publication Type'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.y}</b>'
@@ -14,6 +24,7 @@ $(function () {
         plotOptions: {
             pie: {
                 allowPointSelect: true,
+                animation: false,
                 cursor: 'pointer',
                 dataLabels: {
                     enabled: true,
@@ -23,71 +34,61 @@ $(function () {
                 }
             }
         },
-        series: [{
-            type: 'pie',
-            name: 'Publication Type Percentage',
-            data: [
-                
-                {name:'Journal Paper', y:6321},
-                
-                {name:'None', y:3266},
-                
-                {name:'Conference Paper', y:492},
-                
-                {name:'Book Chapter', y:52},
-                
-                {name:'Book', y:6},
-                
-            ]
-        }],
         credits: {
             enabled: false
         },
-    });
+    };
+    
+    var chart = new Chart('containerDistType', opts, series);
+    chart.create();    
 });
 
 $(function () {
-    $('#containerDistYear').highcharts({
+    var series = [{
+        name: '# of XD related publications',
+        data: [               
+            {name:'2005', y:21},
+            {name:'2006', y:237},
+            {name:'2007', y:280},
+            {name:'2008', y:289},
+            {name:'2009', y:488},
+            {name:'2010', y:670},
+            {name:'2011', y:757},
+            {name:'2012', y:1128},
+            {name:'2013', y:1718},
+            {name:'2014', y:1998},
+            {name:'2015', y:1561},
+            {name:'2016', y:872},
+            {name:'2017', y:5},
+        ]
+    }];
+    
+    opts = {
         chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
+            type: 'column',
+            animation: false
         },
         title: {
-            text: 'XD Related Publications By Year Published'
+            text: 'XD-Related Publications By Year Published'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.y}</b>'
         },
         xAxis: {
             categories: [
-                
                 '2005',
-                
                 '2006',
-                
                 '2007',
-                
-                '2008',
-                
-                '2009',
-                
-                '2010',
-                
-                '2011',
-                
-                '2012',
-                
-                '2013',
-                
-                '2014',
-                
-                '2015',
-                
-                '2016',
-                
-                '2017',
-                
+                '2008',                
+                '2009',                
+                '2010',                
+                '2011',                
+                '2012',               
+                '2013',                
+                '2014',               
+                '2015',                
+                '2016',                
+                '2017',                
             ],
             labels: {
                 rotation: -45,
@@ -106,104 +107,24 @@ $(function () {
         plotOptions: {
             column: {
                 pointPadding: 0.2,
-                borderWidth: 0
+                borderWidth: 0,
+                animation: false
             }
         },
-        series: [{
-            type: 'column',
-            name: '# of XD related publications',
-            data: [
-                
-                {name:'2005', y:21},
-                
-                {name:'2006', y:237},
-                
-                {name:'2007', y:280},
-                
-                {name:'2008', y:289},
-                
-                {name:'2009', y:488},
-                
-                {name:'2010', y:670},
-                
-                {name:'2011', y:757},
-                
-                {name:'2012', y:1128},
-                
-                {name:'2013', y:1718},
-                
-                {name:'2014', y:1998},
-                
-                {name:'2015', y:1561},
-                
-                {name:'2016', y:872},
-                
-                {name:'2017', y:5},
-                
-            ]
-        }],
         credits: {
             enabled: false
         },
-    });
+    };
+    
+    var chart = new Chart('containerDistYear', opts, series);
+    chart.create();
 });
 
 $(function() {
     $.get('https://raw.githubusercontent.com/cloudmesh/metric/master/report/data-fos.csv', function(csv) {
         var chartData = CSV2JSON(csv.replace('fos', 'name').replace('# of Pubs','data'));
-        chartData = chartData.map(function(d){
-            return {name: d['name'], data: [parseInt(d['data'])]};
-        });
-        
-        chartData.sort(function(a, b){
-            return b['data'] - a['data'];
-        });
-        
-        chartData = chartData.slice(0, 20);
-
-        var categories = chartData.map(function(d){
-            return d['name'];
-        })   
-        
-        var newData = [];
-        for(var i = 0; i < chartData.length; i++){
-            newData.push(chartData[i]['data'][0]);
-        }
-        newData = [{name: 'Publications', data: newData}];
-    
-        Highcharts.chart('fosChart', {
-            chart: {
-                type: 'bar'
-            },
-            
-            legend: {
-                enabled: false
-            },
-            
-            title: {
-                text: 'Publications by Field'
-            },
-            
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-            
-            xAxis: {
-                categories: categories
-            },
-            
-            yAxis: {
-                title: {
-                    text: 'Publications'
-                }
-            },
-            
-            series: newData
-        }); 
+        var chart = new BarChart('fosChart', chartData, 'XD-Related Publications by Field');
+        chart.draw();
     });
 });
 
@@ -232,8 +153,12 @@ $(function() {
 	    }
       
         Highcharts.mapChart('orgsStateMap', {
+            chart: {
+                animation: false
+            },
+            
             title: {
-                text: 'Number of Publications by State'
+                text: 'XD-Related Publications by State'
             },
 
             mapNavigation: {
@@ -250,6 +175,12 @@ $(function() {
                     [0.67, '#4444FF'],
                     [1, '#000022']
                 ],
+            },
+            
+            plotOptions: {
+                series: {
+                    animation: false
+                }
             },
 
             series: [{
@@ -270,59 +201,8 @@ $(function() {
         
         // org chart
         var chartData = CSV2JSON(csv.replace('Organization', 'name').replace('# of Pubs','data'));
-        chartData = chartData.map(function(d){
-            return {name: d['name'], data: [parseInt(d['data'])]};
-        });
-        
-        chartData.sort(function(a, b){
-            return b['data'] - a['data'];
-        });
-        
-        chartData = chartData.slice(0, 20);
-
-        var categories = chartData.map(function(d){
-            return d['name'];
-        })   
-        
-        var newData = [];
-        for(var i = 0; i < chartData.length; i++){
-            newData.push(chartData[i]['data'][0]);
-        }
-        newData = [{name: 'Publications', data: newData}];
-        
-        Highcharts.chart('orgsChart', {
-            chart: {
-                type: 'bar'
-            },
-            
-            legend: {
-                enabled: false
-            },
-            
-            title: {
-                text: 'Publications by Organization'
-            },
-            
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-            
-            xAxis: {
-                categories: categories
-            },
-            
-            yAxis: {
-                title: {
-                    text: 'Publications'
-                }
-            },
-            
-            series: newData
-        });
+        var chart = new BarChart('orgsChart', chartData, 'XD-Related Publications by Organization');
+        chart.draw();
     });
 });
 
@@ -385,7 +265,6 @@ function CSVToArray(strData, strDelimiter) {
     return (arrData);
 }
 
-// need js object, not json
 function CSV2JSON(csv) {
     var array = CSVToArray(csv);
     var objArray = [];
@@ -401,7 +280,7 @@ function CSV2JSON(csv) {
     var str = json.replace(/},/g, "},\r\n");
 
     return str;*/
-    return objArray;
+    return objArray; // need js object, not json
 }
 
 function collapse_data(json){
@@ -427,6 +306,122 @@ function collapse_data(json){
 	}
     
     return data;
+}
+
+function BarChart(container, data, title, paginated = true){
+    this.maxItems = 10;
+    this.container = container;
+    
+    chartData = data.map(function(d){
+        return {name: (d['name'] ? d['name'] : 'Not Specified'), data: parseInt(d['data'])};
+    });
+    
+    chartData.sort(function(a, b){
+        return b['data'] - a['data'];
+    });
+    
+    var d = [];
+    var c = {};
+    var numbers = [];
+    for(var i = 0; i < chartData.length; i++){
+        c = chartData[i];
+        numbers = numbers.concat(c['data']);
+        d.push([c['name'], c['data']]);
+    }
+    series = [{name: 'Publications', data: d}];
+    
+    var max = roundHighestUp(numbers);
+    
+    this.options = {
+        chart: {
+            type: 'bar',
+            animation: false,
+            marginLeft: 250
+        },
+        
+        legend: {
+            enabled: false
+        },
+        
+        title: {
+            text: title
+        },
+        
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                },
+                animation: false
+            }
+        },
+        
+        xAxis: {
+            type: 'category'
+        },
+        
+        yAxis: {
+            title: {
+                text: 'Publications'
+            },
+            min: 0,
+            max: max
+        }
+    };
+    
+    this.options.series = JSON.parse(JSON.stringify(series)); //deep copy
+    this.series = series;
+    
+    this.draw = function(){
+        if(!paginated){
+            Highcharts.chart(this.container, this.options);
+        } else {
+            var chartObj = this;
+            this.currentPage = 1;
+            this.options.series[0].data = this.series[0].data.slice(0, this.maxItems);
+            
+            Highcharts.chart(this.container, this.options, function(chart) {
+                var left = chart.renderer.button('\u25B2', chart.plotLeft + chart.plotWidth, chart.plotHeight + chart.plotTop, function(){
+                    var mi = chartObj.maxItems;
+                    var cp = chartObj.currentPage;
+                    if(cp > 1){
+                        
+                        chart.series[0].setData(chartObj.series[0].data.slice((cp - 2) * mi, (cp - 1) * mi), true, false, false);
+                        chartObj.currentPage--;  
+                    } 
+                }).addClass('left').add();
+                left.attr({'x': left.x - left.width - 5, 'y': left.y - left.height * 2});
+
+                var right = chart.renderer.button('\u25BC', chart.plotLeft + chart.plotWidth, chart.plotHeight + chart.plotTop, function() {
+                    var mi = chartObj.maxItems;
+                    var cp = chartObj.currentPage;
+                    if(cp < chartObj.series[0].data.length / mi){
+                        chart.series[0].setData(chartObj.series[0].data.slice(cp * mi, (cp + 1) * mi), true, false, false);
+                        chartObj.currentPage++;
+                    } 
+                }).addClass('right').add();
+                right.attr({'x': right.x - right.width - 5, 'y': right.y - right.height});
+            });
+        }
+    }
+}
+
+function Chart(container, options, series){
+    this.container = container;
+    this.options = options;
+    this.series = series;    
+    this.options.series = series;
+    
+    this.create = function(){
+        Highcharts.chart(this.container, this.options);
+    }
+}
+
+function roundHighestUp(numbers){
+    var highest = Math.max.apply(null, numbers);
+    var length = highest.toString().length
+    var off = Math.pow(10, (length - 2));
+    return Math.ceil(highest / off) * off;
 }
 
 /*
@@ -475,4 +470,3 @@ function populateTable(id, title, csv, fields){
     
     $div.append($table);
 }
-
