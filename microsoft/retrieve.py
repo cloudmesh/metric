e3a8966dc9de46be0767a915e422d7a6c8565ac0
Tim Whitson <whitstd@gmail.com>
2017-04-09 23:07:22 -0400

    add Microsoft API python script

---
+++ microsoft/retrieve.py
@@ -0,0 +1,35 @@
+import requests
+
+class AK_API:
+    def get_credentials(self):
+        key = raw_input('What is your API key? ')
+        return key
+        
+    def get_author(self):
+        author = raw_input('What is the name of the author to search for? ')
+        return author.lower()
+        
+    def evaluate(self):
+        query = {}
+        
+        key = self.get_credentials()
+        query['author'] = self.get_author()
+        
+        url = "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=Composite(AA.AuN=='{author}')&attributes=Ti,Y,CC,AA.AuN,AA.AuId".format(**query)
+        
+        headers = {
+            'Ocp-Apim-Subscription-Key': key,
+        }       
+        
+        print 'Performing query for author: {author}.'.format(**query)
+        r = requests.get(url, headers = headers)
+        
+        print 'Saving result'
+        with open('result.json', 'w+') as f:
+            f.write(r.content)
+            
+        print 'Compelete.'
+        
+if(__name__ == '__main__'):
+    a = AK_API()
+    a.evaluate()
