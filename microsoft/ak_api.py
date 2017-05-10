3b79ff70d7d4cf4b3949f68010ef2b630935a050
Tim Whitson <tdwhitso@indiana.edu>
2017-05-10 14:54:58 -0400

    update fos hierarchy

--- microsoft/ak_api.py
+++ microsoft/ak_api.py
@@ -2,13 +2,14 @@
 
 Usage:
   ak_api.py evaluate <year> [--offset=<offset>]
+  ak_api.py parents
   ak_api.py citations
 
 Options:
   year:   year to retrieve publications for
 '''
 
-import time, os.path, ConfigParser
+import time, os.path, ConfigParser, csv
 import requests, pymongo, numpy
 from docopt import docopt
 from doc_parse import DocParser
@@ -88,7 +89,7 @@ class AK_API:
             
             data = {}
             data['expr'] = 'Y={year}'.format(year = year)
-            data['attributes'] = 'Id,Ti,Y,CC,AA.AuN,AA.AuId,AA.AfN,F.FN,J.JN'
+            data['attributes'] = 'Id,Ti,Y,L,D,CC,ECC,AA.AuN,AA.AuId,AA.AfN,AA.AfId,AA.S,F.FId,F.FN,J.JId,J.JN,C.Cid,C.CN,RId'
             data['count'] = offset
             data['offset'] = count * offset
             
@@ -100,7 +101,7 @@ class AK_API:
                 'Ocp-Apim-Subscription-Key': key,
             }       
             
-            print 'Getting items at {number} ...'.format(number = count * offset)
+            print 'Getting items from {start} to {finish} ...'.format(start = count * offset + 1, finish = (count + 1) * offset)
             r = requests.get(url, headers = headers)
             
             result = r.json()
@@ -133,27 +134,120 @@ class AK_API:
     def citations(self):
         '''Get citation count by FOS
         '''
-        print 'Retrieving citation count ...'
+        print 'Retrieving citation count ...'  
         
         collection = self.db[self.collection_name]       
-        result = collection.find({}, {'_id': 0, 'CC': 1, 'F.FN': 1})
+        result = collection.find({}, {'_id': 0, 'CC': 1, 'F.parent': 1})
         
         citations = {}
         
         for pub in result:
             if('CC' in pub and 'F' in pub):
+                fields = []
+                
                 for f in pub['F']:
-                    field = f['FN']
-                    if field in citations:
-                        citations[field].append(pub['CC'])
+                    if('parent' in f):
+                        parent = f['parent']
+                        fields.append(parent)
+                    
+                fields = list(set(fields))
+                
+                for f in fields:
+                    if f in citations:
+                        citations[f].append(pub['CC'])
                     else:
-                        citations[field] = [pub['CC']]
+                        citations[f] = [pub['CC']]
         
         print 'Computing averages ...'
-        # get averages       
         citations = {k: numpy.mean(v) for k, v in citations.iteritems()}
         
+        print citations
         return citations
+        
+    def parents(self):
+        '''Add field of study parents
+        Warning:
+            Microsoft has duplicate entries for any given field name, field names from FieldsOfStudy.txt do not match current field names retrieved from API
+        '''
+        print 'Adding parents to fields ...'
+        
+        records = self.db[self.collection_name].find()
+        
+        with open('FieldsOfStudy.txt') as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            fields = {f['id']: f['name'].lower() for f in reader}
+            ids = dict(zip(fields.values(), fields.keys()))
+        
+        with open('FieldOfStudyHierarchy.txt') as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            
+            parents = {}
+            children = {}
+            
+            for row in reader:
+                parent_id = row['parent_id']
+                if(row['parent_level'] == 'L0' and parent_id not in parents):
+                    parents[parent_id] = parent_id
+                    children[parent_id] = {'level': 'L0'}
+          
+                child = children.get(row['child_id'])
+                if(child):
+                    child['parents'][row['parent_id']] = row['prob']
+                else:
+                    children[row['child_id']] = {'parents': {row['parent_id']: row['prob']}, 'level': row['child_level']}
+                    
+                    
+            def get_prob(parent, prob, level = 1):
+                c_parents = children[parent].get('parents')
+                if(not c_parents):
+                    print prob
+                    return prob
+                for c_parent, c_prob in c_parents.items():
+                    get_prob(c_parent, float(c_prob) * float(prob), level + 1)
+
+            for child, info in children.iteritems():
+                if('parents' in info):
+                    for parent, prob in info['parents'].items():
+                        print get_prob(parent, prob)
+                    exit()
+                        
+            '''parents = {}
+            for row in reader:
+                child = parents.get(row['child_id']) or {}
+                
+                if(child):
+                    if(row['parent_level'] == 'L0' and row['prob'] > child['prob']):
+                        child['parent_id'] = row['parent_id']
+                        child['level'] = row['parent_level']
+                        child['prob'] = row['prob']
+                else:
+                    if(row['parent_level'] == 'L0'):
+                        child['parent_id'] = row['parent_id']
+                        child['level'] = row['parent_level']
+                        child['prob'] = row['prob']
+                        parents[row['child_id']] = child'''
+                        
+            
+                        
+                # if not child: try again with one of its parents
+            
+        for record in records:
+            if('F' in record):
+                for field in record['F']:
+                    field_name = field['FN']                            
+                    field_id = ids.get(field_name)
+                            
+                    if(field_id):
+                        parent = parents.get(field_id)
+                        if(parent):
+                            field['parent'] = fields.get(parent)
+                            
+                self.db[self.collection_name].update(
+                    {'_id': record['_id']},
+                    {'$set': {'F': record['F']}}
+                )
+                                         
+        return records
         
 if(__name__ == '__main__'):
     a = AK_API()
